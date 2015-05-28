1d12df567da7c0df2025f4d90103d5d0da1fdee2
toyama-h <toyama-h@ipros.jp>
2015-05-28 19:49:10 +0900

    add test.

--- test/plugin/test_in_ping_port.rb
+++ test/plugin/test_in_ping_port.rb
@@ -11,17 +11,49 @@ class PingPortInputTest < Test::Unit::TestCase
 
   def test_configure_full
     d = create_driver %q{
-      tag test
+      tag ping_port.exsample
+      host web01
+      port 24224,80
+      timeout 10
       interval 10m
+      retry_count 5
     }
 
-    assert_equal 'test', d.instance.tag
+    assert_equal 'ping_port.exsample', d.instance.tag
     assert_equal 10 * 60, d.instance.interval
+    assert_equal 10, d.instance.timeout
+    assert_equal ["24224", "80"], d.instance.instance_variable_get(:@ports)
+    assert_equal ({"24224" => 0, "80" => 0}), d.instance.instance_variable_get(:@state)
   end
 
   def test_configure_error_when_config_is_empty
     assert_raise(Fluent::ConfigError) do
-      create_driver ''
+      create_driver  %q{
+        tag ping_port.exsample
+        host web01
+        timeout 10
+        interval 10m
+        retry_count 5
+      }
+    end
+  end
+
+  def test_emit
+    d = create_driver %q{
+      tag ping_port.exsample
+      host localhost
+      port 9999
+      timeout 10
+      interval 1s
+      retry_count 2
+    }
+
+    d.run do
+      sleep 3
+      emits = d.emits
+      assert_equal true, emits.length > 0
+      assert_equal "ping_port.exsample", emits[0].first
+      assert_equal ({"message"=>"localhost:9999 Connect Error."}), emits[0].last
     end
   end
 end
